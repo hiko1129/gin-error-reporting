ec9967f0d4818b8a59ea85549a7e87e76ca23734
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2020-05-14 17:24:48 +0900

    fixed

--- reporting.go
+++ reporting.go
@@ -1,6 +1,8 @@
 package reporting
 
 import (
+	"log"
+
 	"cloud.google.com/go/errorreporting"
 	"github.com/gin-gonic/gin"
 )
@@ -8,7 +10,10 @@ import (
 // New returns a new gin middleware which send error report to gcp error reporting
 func New(client *errorreporting.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		c.Next()
+
 		err := c.Errors.Last()
+		log.Println(err)
 
 		client.Report(errorreporting.Entry{
 			Error: err,
